7016e785b164a85f341bdf947a4b51a17056e2d2
hbl9 <kostyapatershin86@gmail.com>
2026-04-02 20:47:09 +0500

    LineManager

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float _speed;
-    [SerializeField] float _stepWidth;
+    [SerializeField] LineManager _lineManager;
+    [SerializeField] int _currentLineId;
 
     void Update()
     {
         transform.position += _speed * Time.deltaTime * transform.forward;
 
-        if (Input.GetKeyDown(KeyCode.A))
-            transform.position -= transform.right * _stepWidth;
+        if (Input.GetKeyDown(KeyCode.D) && _currentLineId > 0)
+            transform.position = new(transform.position.x, transform.position.y, _lineManager.Lines[--_currentLineId]);
 
-        if (Input.GetKeyDown(KeyCode.D))
-            transform.position += transform.right * _stepWidth;
+        if (Input.GetKeyDown(KeyCode.A) && _currentLineId < _lineManager.Lines.Count)
+            transform.position = new(transform.position.x, transform.position.y, _lineManager.Lines[++_currentLineId]);
     }
 }

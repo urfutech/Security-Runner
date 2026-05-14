6ce7f2f364aa0c1c6db608fc20afe78b2f12591d
hbl9 <kostyapatershin86@gmail.com>
2026-05-14 12:30:11 +0500

    Bug fix road

--- Assets/Scripts/EndlessField.cs
+++ Assets/Scripts/EndlessField.cs
@@ -5,17 +5,21 @@ public class EndlessField : MonoBehaviour
     [SerializeField] GameObject _road1;
     [SerializeField] GameObject _road2;
 
+    Transform _playerTransform;
+
     void Start()
     {
         if (_road1 == null) Debug.LogError("Road1 не назначен");
         if (_road2 == null) Debug.LogError("Road2 не назначен");
+
+        _playerTransform = GameManager.Instance.PlayerTransform;
     }
 
     void Update()
     {
-        if (transform.position.x >= _road1.transform.position.x + 190)
+        if (_playerTransform.position.x >= _road1.transform.position.x + 190)
             _road1.transform.position = new Vector3(_road2.transform.position.x + 200, 0, 0);
-        if (transform.position.x >= _road2.transform.position.x + 190)
+        if (_playerTransform.position.x >= _road2.transform.position.x + 190)
             _road2.transform.position = new Vector3(_road1.transform.position.x + 200, 0, 0);
     }
 }
